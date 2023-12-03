45ff287b4d274c3c12b7cb49cc23e05bf48d7388
kev-meca <ka.martinezm12@uniandes.edu.co>
2023-12-03 10:31:51 -0500

    Update Main.R

--- Scripts /Main.R
+++ Scripts /Main.R
@@ -5,27 +5,37 @@
 
 
 # 1| Preparacion ----------------------------------------------------------
-rm(list = ls())
-graphics.off()
-options(scipen = 999)
-set.seed(123)  # Replicabilidad en las simulaciones.
+# Limpia el entorno y ajustes gráficos
+ rm(list = ls())
+ graphics.off()
+ options(scipen = 999)
+ set.seed(123)  # Replicabilidad en las simulaciones.
+
 
 # 1.1| Librerias ----------------------------------------------------------
+
+
+# Lista de librerías a instalar si no están cargadas
 librerias <- c(
   'here', 'tidyverse', 'tidymodels', 'conflicted', 'xtable',
   'gtsummary', 'gt', 'stringr', 'extrafont', 'xgboost',
   'lightgbm', 'bonsai', 'vip', 'randomForest', 'rpart',
   'baguette', 'themis', 'doParallel'
 )
+
+# Instala las librerías faltantes
 noInstaladas <- librerias[!(librerias %in% rownames(installed.packages()))]
 
 if (length(noInstaladas)) {
   install.packages(noInstaladas)
 }
 
+# Carga las librerías
 invisible(sapply(librerias, function(pkg) {
   library(pkg, character.only = TRUE, quietly = TRUE)
 }))
+
+# Configuración de conflictos
 loadfonts(device = 'win')
 conflict_prefer(name = 'filter', winner = 'dplyr')
 conflict_prefer(name = 'slice', winner = 'dplyr')
@@ -35,8 +45,6 @@ conflict_prefer(name = 'setdiff', winner = 'base')
 conflict_prefer(name = 'starts_with', winner = 'tidyselect')
 
 # 1.2| Directorios ---------------------------------------------------------
-
-# Directorio base
 
 # Elegir un archivo de manera interactiva para obtener el directorio base
 file_selected <- file.choose()
@@ -56,9 +64,12 @@ if (!dir.exists(base_dir)) {
   stop("La ejecución del script se detuvo debido a un error en la ruta del directorio base.")
 }
 
+# 1.3| Funciones ----------------------------------------------------------
 
-# Cargar funciones
-source(file.path(codigo_dir, "Functions/Cleaning.R"), encoding = "UTF-8")
+# Cargar funciones de limpieza
+source(file.path(base_dir, "Cleaning.R"), encoding = "UTF-8")
+
+# 1.4| Guardar y cargar resultados ------------------------------------------
 
 # Guardar resultados
 primera_vez <- FALSE
